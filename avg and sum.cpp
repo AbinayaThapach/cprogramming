91b7437aa5ef959de0bc47761de12e27b487f1cd
AbinayaThapach <PAS081BCT004@wrc.edu.np>
2024-12-17 19:56:17 +0545

    Add files via upload

---
+++ avg and sum.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{ int a,b,sum,avg;
+printf("Enter the value of a and b");
+scanf("%d %d ",&a,&b);
+sum=a+b;
+avg=sum/2;
+printf("the sum and average is %d %d ",sum,avg);
+}
