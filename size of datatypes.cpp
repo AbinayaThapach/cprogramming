cde0f9a1655c106faf3f567451f941879228fd3e
AbinayaThapach <PAS081BCT004@wrc.edu.np>
2025-01-03 19:21:47 +0545

    Add files via upload

---
+++ size of datatypes.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	printf("the size of int is %d",sizeof (int));
+	printf("the size of float is %d",sizeof (float));
+	printf("the size of char is %d",sizeof(char));
+	getch();
+}
