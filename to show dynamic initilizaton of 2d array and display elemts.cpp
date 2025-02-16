ea44621ab0d9719d030a768fce20bdc8a5744640
AbinayaThapach <PAS081BCT004@wrc.edu.np>
2025-02-16 14:38:18 +0545

    Add files via upload

---
+++ to show dynamic initilizaton of 2d array and display elemts.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int marks[2][2],i,j,k;
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			
+			
+				scanf("%d",&marks[i][j]);
+			
+		}}
+	
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+		
+			
+				printf("%d ",marks[i][j]);
+			
+			
+		}
+	}}
